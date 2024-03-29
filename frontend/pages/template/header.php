af51de3854170383aa4ba094eb596d7260f2db4e
Kirtika <kirtika.sn@gmail.com>
2024-03-29 23:08:35 +0100

    add header

--- frontend/pages/template/header.php
+++ frontend/pages/template/header.php
@@ -9,7 +9,72 @@
     <link rel="stylesheet" href="../style.css">
     <link rel="icon" type="image/x-icon" href="../assets/favicon.ico">
   </head>
+
   <body>
+    <header>
+      <div class="row p-3 d-flex justify-content-center align-items-center">
+        <div class="col">
+          <!-- Barre de navigation -->
+          <nav class="navbar bg-body-tertiary">
+            <div class="container-fluid">
+              <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbarLight">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbarLight">
+                <div class="offcanvas-header">
+                  <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
+                </div>
+                <div class="offcanvas-body">
+                  <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                    <li class="nav-item">
+                      <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                      <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Espace bailleur
+                      </a>
+                      <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li>
+                          <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                      </ul>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Devenir Premium</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#">Contact</a>
+                    </li>
+                  </ul>
+                  <form class="d-flex mt-3" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
+        <div class="col-5 text-center">
+          <!-- Titre -->
+          <img src="../assets/logos/title_dark.png" width="200px">
+        </div>
+        <div class="col text-end">
+          <!-- Bouton de connexion/inscription -->
+          <button type="button" class="btn btn-dark">Connexion</button>
+          <button type="button" class="btn btn-dark">Inscription</button>
+        </div>
+      </div>
+      <!-- Séparateur -->
+      <div class="brown-box"></div>
+    </header>
+
+
+
+    
   <!-- 
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
