20921620d17c330848855ef716db1fa4bd97c73b
Kirtika <kirtika.sn@gmail.com>
2024-04-15 10:56:16 +0200

    fixe db connection

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const connection = require('./connection.js');
+const connection = require('./db-connection.js');
 
 
 /* ----------------------------------------------------------
