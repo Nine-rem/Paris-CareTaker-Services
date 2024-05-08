ac4be3e6f9ffc2b85141ecaafa03ca37a7f4165b
Nine-rem <alexis2000.te@hotmail.fr>
2024-05-08 14:19:09 +0200

    header

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,48 +1,125 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+const logo = require('../assets/logos/icon_dark.png');
+
 
 function Header() {
     return (
         <>
-            <header>
-                <div className="row p-3 d-flex justify-content-center align-items-center">
-                    <div className="col-2">
-                        <nav className="navbar bg-body-tertiary">
-                            <div className="container-fluid">
-                                <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbarLight">
-                                    <span className="navbar-toggler-icon"></span>
-                                </button>
-                                <div className="offcanvas offcanvas-end" tabIndex="-1" id="offcanvasNavbarLight">
-                                    <div className="offcanvas-header">
-                                        <img src="../assets/logos/icon_dark.png" alt="Icon" className="img-fluid" width="50px"/>
-                                        <button type="button" className="btn-close" data-bs-dismiss="offcanvas"></button>
-                                    </div>
-                                    <div className="offcanvas-body">
-                                        <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-                                            <li className="nav-item">
-                                                <a className="nav-link active" aria-current="page" href="#">Accueil</a>
-                                            </li>
-                                            {/* Additional menu items */}
-                                        </ul>
-                                        <form className="d-flex mt-3" role="search">
-                                            <input className="form-control me-2" type="search" placeholder="Tapez votre recherche" aria-label="Search"/>
-                                            <button className="btn btn-dark" type="submit">Rechercher</button>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </nav>
+    <header>
+        <div className="row p-3 d-flex justify-content-center align-items-center">
+            <div className="col-2">
+            {/* <!-- Barre de navigation --> */}
+            <nav className="navbar bg-body-tertiary">
+                <div className="container-fluid">
+                <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbarLight">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbarLight">
+                    <div className="offcanvas-header">
+                    {/* <!-- Image du menu --> */}
+                    <img src={logo} alt="Icon" className="img-fluid" width="50px"></img>
+                    <button type="button" className="btn-close" data-bs-dismiss="offcanvas"></button>
                     </div>
-                    <div className="col text-center">
-                        <img src="../assets/logos/title_dark.png" width="200px"/>
-                    </div>
-                    <div className="col-2 text-end d-none d-md-block">
-                        <button type="button" className="btn btn-dark">Connexion</button>
-                        <button type="button" className="btn btn-dark">Inscription</button>
+                    <div className="offcanvas-body">
+                    <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                        <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">Accueil</a>
+                        </li>
+                        <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Votre compte
+                        </a>
+                        <ul className="dropdown-menu">
+                            <li><a className="dropdown-item" href="#">Votre profil</a></li>
+                            <li><a className="dropdown-item" href="#">Vos documents</a></li>
+                            <li><a className="dropdown-item" href="#">Vos comptes</a></li>
+                            <li><a className="dropdown-item" href="#">Vos réservations</a></li>
+                            <li><a className="dropdown-item" href="#">Logements favoris</a></li>
+                        </ul>
+                        </li>
+                        <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Espace bailleur
+                        </a>
+                        <ul className="dropdown-menu">
+                            <li><a className="dropdown-item" href="#">Nouveau bien</a></li>                        
+                            <li><a className="dropdown-item" href="#">Vos biens</a></li>
+                            <li><a className="dropdown-item" href="#">Réservations</a></li>
+                            <li><a className="dropdown-item" href="#">Vos interventions</a></li>
+                        </ul>
+                        </li>
+                        <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Espace prestataire
+                        </a>
+                        <ul className="dropdown-menu">
+                            <li><a className="dropdown-item" href="#">Nouvelle prestations</a></li>
+                            <li><a className="dropdown-item" href="#">Vos prestations</a></li>
+                            <li><a className="dropdown-item" href="#">Vos interventions</a></li>
+                        </ul>
+                        </li>
+                        <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Espace administrateur
+                        </a>
+                        <ul className="dropdown-menu">
+                            <li><a className="dropdown-item" href="#">Utilisateurs</a></li>
+                            <li><a className="dropdown-item" href="#">Biens</a></li>
+                            <li><a className="dropdown-item" href="#">Services</a></li>
+                            <li><a className="dropdown-item" href="#">Demandes en attente</a></li>
+                            <li><a className="dropdown-item" href="#">Documents</a></li>
+                            <li><a className="dropdown-item" href="#">Gestion technique</a></li>
+                        </ul>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="#">Trouvez un logement</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="#">Trouvez un service</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="#">Messagerie</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="#">Contact</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="#">Devenir Premium</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="#">Connectez-vous</a>
+                        </li>
+                        <li className="nav-item">
+                        <a className="nav-link" href="#">Nous rejoindre</a>
+                        </li>
+                    </ul>
+                    {/* <!-- Barre de recherche --> */}
+                    <form className="d-flex mt-3" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Tapez votre recherche" aria-label="Search"></input>
+                        <button className="btn btn-dark" type="submit">Rechercher</button>
+                    </form>
                     </div>
                 </div>
-                <div className="brown-separator"></div>
-            </header>
-        </>
+                </div>
+            </nav>
+            </div>
+            <div className="col text-center">
+            {/* <!-- Titre --> */}
+            <img src="../assets/logos/title_dark.png" width="200px"></img>
+            </div>
+            <div className="col-2 text-end d-none d-md-block">
+            {/* <!-- Bouton de connexion/inscription --> */}
+            <button type="button" className="btn btn-dark">Connexion</button>
+            <button type="button" className="btn btn-dark">Inscription</button>
+            </div>
+        </div>
+        {/* <!-- Séparateur --> */}
+        <div className="brown-separator"></div>
+        </header>
+    </>
     );
 }
 
