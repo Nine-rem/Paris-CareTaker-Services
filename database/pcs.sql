99b88d445f40bc15a587879cc9cd0979a257018e
Nine-rem <alexis2000.te@hotmail.fr>
2024-06-12 20:05:10 +0200

    Modified sql

--- database/pcs.sql
+++ database/pcs.sql
@@ -788,89 +788,82 @@ ADD CONSTRAINT `pcs_photo_ibfk_2` FOREIGN KEY (`photo_bien_id`) REFERENCES `pcs_
 
 -- Biens
 -- Insertion de biens
-INSERT INTO `pcs_bien` (`nom_bien`, `statut_bien`, `adresse_bien`, `cp_bien`, `ville_bien`, `type_location_bien`, `capacite_bien`, `surface_bien`, `animal_ok_bien`, `PMR_ok_bien`, `description_bien`, `tarif_bien`, `bailleur`, `agence_principale_bien`) VALUES
-('Villa avec vue sur la mer', 0, '5 rue Goury', '77654', 'Villeneuve la Garenne', 'Logement entier', 9, 150.00, 1, 1, 'Villa luxueuse avec vue sur la mer.', 130.00, 2, 1),
-('Appartement cosy', 0, '10 rue des Lilas', '75000', 'Paris', 'Appartement', 4, 60.00, 0, 0, 'Appartement confortable au centre-ville.', 80.00, 2, 1),
-('Chalet en montagne', 0, '12 chemin des Neiges', '73000', 'Chambéry', 'Chalet', 6, 120.00, 1, 1, 'Chalet chaleureux au cœur des montagnes.', 150.00, 2, 1),
-('Maison de campagne', 0, '8 rue de la Forêt', '45000', 'Orléans', 'Maison', 5, 110.00, 1, 0, 'Maison tranquille entourée de nature.', 100.00, 2, 1),
-('Studio moderne', 0, '3 avenue des Champs', '06000', 'Nice', 'Studio', 2, 30.00, 0, 0, 'Studio moderne et bien équipé.', 70.00, 2, 1);
-
--- Récupération des IDs générés pour les biens
-SET @bien_id1 = LAST_INSERT_ID();
-SET @bien_id2 = @bien_id1 + 1;
-SET @bien_id3 = @bien_id1 + 2;
-SET @bien_id4 = @bien_id1 + 3;
-SET @bien_id5 = @bien_id1 + 4;
+INSERT INTO `pcs_bien` (1,`nom_bien`, `statut_bien`, `adresse_bien`, `cp_bien`, `ville_bien`, `type_location_bien`, `capacite_bien`, `surface_bien`, `animal_ok_bien`, `PMR_ok_bien`, `description_bien`, `tarif_bien`, `bailleur`, `agence_principale_bien`) VALUES
+(2,'Villa avec vue sur la mer', 0, '5 rue Goury', '77654', 'Villeneuve la Garenne', 'Logement entier', 9, 150.00, 1, 1, 'Villa luxueuse avec vue sur la mer.', 130.00, 2, 1),
+(3,'Appartement cosy', 0, '10 rue des Lilas', '75000', 'Paris', 'Appartement', 4, 60.00, 0, 0, 'Appartement confortable au centre-ville.', 80.00, 2, 1),
+(4,'Chalet en montagne', 0, '12 chemin des Neiges', '73000', 'Chambéry', 'Chalet', 6, 120.00, 1, 1, 'Chalet chaleureux au cœur des montagnes.', 150.00, 2, 1),
+(5,'Maison de campagne', 0, '8 rue de la Forêt', '45000', 'Orléans', 'Maison', 5, 110.00, 1, 0, 'Maison tranquille entourée de nature.', 100.00, 2, 1),
+(6,'Studio moderne', 0, '3 avenue des Champs', '06000', 'Nice', 'Studio', 2, 30.00, 0, 0, 'Studio moderne et bien équipé.', 70.00, 2, 1);
 
 -- Pièces pour chaque bien
 -- Villa avec vue sur la mer
 INSERT INTO `pcs_piece` (`nom_piece`, `description_piece`, `est_privatif_piece`, `bien_piece`, `surface_piece`, `type_piece`) VALUES
-('Salon', 'Salon spacieux avec vue sur la mer.', 1, @bien_id1, 50.00, 1),
-('Cuisine', 'Cuisine entièrement équipée.', 1, @bien_id1, 20.00, 3),
-('Chambre double', 'Chambre avec lit double et balcon.', 1, @bien_id1, 15.00, 2),
-('Salle de bain', 'Salle de bain avec baignoire et douche.', 1, @bien_id1, 10.00, 5);
+('Salon', 'Salon spacieux avec vue sur la mer.', 1, 1, 50.00, 1),
+('Cuisine', 'Cuisine entièrement équipée.', 1, 1, 20.00, 3),
+('Chambre double', 'Chambre avec lit double et balcon.', 1, 1, 15.00, 2),
+('Salle de bain', 'Salle de bain avec baignoire et douche.', 1, 1, 10.00, 5);
 
 -- Appartement cosy
 INSERT INTO `pcs_piece` (`nom_piece`, `description_piece`, `est_privatif_piece`, `bien_piece`, `surface_piece`, `type_piece`) VALUES
-('Salon', 'Salon cosy et lumineux.', 1, @bien_id2, 25.00, 1),
-('Cuisine', 'Petite cuisine ouverte.', 1, @bien_id2, 10.00, 3),
-('Chambre double', 'Chambre avec lit double.', 1, @bien_id2, 12.00, 2),
-('Salle de bain', 'Salle de bain moderne.', 1, @bien_id2, 8.00, 5);
+('Salon', 'Salon cosy et lumineux.', 1, 2, 25.00, 1),
+('Cuisine', 'Petite cuisine ouverte.', 1, 2, 10.00, 3),
+('Chambre double', 'Chambre avec lit double.', 1, 2, 12.00, 2),
+('Salle de bain', 'Salle de bain moderne.', 1, 2, 8.00, 5);
 
 -- Chalet en montagne
 INSERT INTO `pcs_piece` (`nom_piece`, `description_piece`, `est_privatif_piece`, `bien_piece`, `surface_piece`, `type_piece`) VALUES
-('Salon', 'Salon avec cheminée.', 1, @bien_id3, 40.00, 1),
-('Cuisine', 'Cuisine rustique.', 1, @bien_id3, 15.00, 3),
-('Chambre double', 'Chambre avec vue sur les montagnes.', 1, @bien_id3, 20.00, 2),
-('Salle de bain', 'Salle de bain avec douche.', 1, @bien_id3, 10.00, 5);
+('Salon', 'Salon avec cheminée.', 1, 3, 40.00, 1),
+('Cuisine', 'Cuisine rustique.', 1, 3, 15.00, 3),
+('Chambre double', 'Chambre avec vue sur les montagnes.', 1, 3, 20.00, 2),
+('Salle de bain', 'Salle de bain avec douche.', 1, 3, 10.00, 5);
 
 -- Maison de campagne
 INSERT INTO `pcs_piece` (`nom_piece`, `description_piece`, `est_privatif_piece`, `bien_piece`, `surface_piece`, `type_piece`) VALUES
-('Salon', 'Salon confortable avec cheminée.', 1, @bien_id4, 35.00, 1),
-('Cuisine', 'Cuisine équipée et spacieuse.', 1, @bien_id4, 20.00, 3),
-('Chambre double', 'Chambre avec lit double.', 1, @bien_id4, 18.00, 2),
-('Salle de bain', 'Salle de bain moderne.', 1, @bien_id4, 12.00, 5);
+('Salon', 'Salon confortable avec cheminée.', 1, 4, 35.00, 1),
+('Cuisine', 'Cuisine équipée et spacieuse.', 1, 4, 20.00, 3),
+('Chambre double', 'Chambre avec lit double.', 1, 4, 18.00, 2),
+('Salle de bain', 'Salle de bain moderne.', 1, 4, 12.00, 5);
 
 -- Studio moderne
 INSERT INTO `pcs_piece` (`nom_piece`, `description_piece`, `est_privatif_piece`, `bien_piece`, `surface_piece`, `type_piece`) VALUES
-('Salon', 'Espace salon moderne.', 1, @bien_id5, 15.00, 1),
-('Cuisine', 'Cuisine ouverte et fonctionnelle.', 1, @bien_id5, 8.00, 3),
-('Salle de bain', 'Salle de bain avec douche.', 1, @bien_id5, 7.00, 5);
+('Salon', 'Espace salon moderne.', 1, 5, 15.00, 1),
+('Cuisine', 'Cuisine ouverte et fonctionnelle.', 1, 5, 8.00, 3),
+('Salle de bain', 'Salle de bain avec douche.', 1, 5, 7.00, 5);
 
 -- Photos pour chaque pièce
 -- Villa avec vue sur la mer
 INSERT INTO `pcs_photo` (`nom_photo`, `description_photo`, `chemin_photo`, `piece_photo`, `est_couverture`, `photo_bien_id`) VALUES
-('salon1', 'Photo du salon avec vue sur la mer.', '/salon1.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Salon' AND bien_piece = @bien_id1), 1, @bien_id1),
-('cuisine1', 'Photo de la cuisine moderne.', '/cuisine1.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Cuisine' AND bien_piece = @bien_id1), 0, @bien_id1),
-('chambre1', 'Photo de la chambre avec balcon.', '/chambre1.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Chambre double' AND bien_piece = @bien_id1), 0, @bien_id1),
-('sdb1', 'Photo de la salle de bain.', '/sdb1.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Salle de bain' AND bien_piece = @bien_id1), 0, @bien_id1);
+('salon1', 'Photo du salon avec vue sur la mer.', '/salon1.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Salon' AND bien_piece = 1), 1, 1),
+('cuisine1', 'Photo de la cuisine moderne.', '/cuisine1.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Cuisine' AND bien_piece = 1), 0, 1),
+('chambre1', 'Photo de la chambre avec balcon.', '/chambre1.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Chambre double' AND bien_piece = 1), 0, 1),
+('sdb1', 'Photo de la salle de bain.', '/sdb1.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Salle de bain' AND bien_piece = 1), 0, 1);
 
 -- Appartement cosy
 INSERT INTO `pcs_photo` (`nom_photo`, `description_photo`, `chemin_photo`, `piece_photo`, `est_couverture`, `photo_bien_id`) VALUES
-('salon2', 'Salon cosy et lumineux.', '/salon2.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Salon' AND bien_piece = @bien_id2), 1, @bien_id2),
-('cuisine2', 'Petite cuisine ouverte.', '/cuisine2.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Cuisine' AND bien_piece = @bien_id2), 0, @bien_id2),
-('chambre2', 'Chambre avec lit double.', '/chambre2.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Chambre double' AND bien_piece = @bien_id2), 0, @bien_id2),
-('sdb2', 'Salle de bain moderne.', '/sdb2.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Salle de bain' AND bien_piece = @bien_id2), 0, @bien_id2);
+('salon2', 'Salon cosy et lumineux.', '/salon2.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Salon' AND bien_piece = 2), 1, 2),
+('cuisine2', 'Petite cuisine ouverte.', '/cuisine2.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Cuisine' AND bien_piece = 2), 0, 2),
+('chambre2', 'Chambre avec lit double.', '/chambre2.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Chambre double' AND bien_piece = 2), 0, 2),
+('sdb2', 'Salle de bain moderne.', '/sdb2.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Salle de bain' AND bien_piece = 2), 0, 2);
 
 -- Chalet en montagne
 INSERT INTO `pcs_photo` (`nom_photo`, `description_photo`, `chemin_photo`, `piece_photo`, `est_couverture`, `photo_bien_id`) VALUES
-('salon3', 'Salon avec cheminée.', '/salon3.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Salon' AND bien_piece = @bien_id3), 1, @bien_id3),
-('cuisine3', 'Cuisine rustique.', '/cuisine3.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Cuisine' AND bien_piece = @bien_id3), 0, @bien_id3),
-('chambre3', 'Chambre avec vue sur les montagnes.', '/chambre3.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Chambre double' AND bien_piece = @bien_id3), 0, @bien_id3),
-('sdb3', 'Salle de bain avec douche.', '/sdb3.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Salle de bain' AND bien_piece = @bien_id3), 0, @bien_id3);
+('salon3', 'Salon avec cheminée.', '/salon3.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Salon' AND bien_piece = 3), 1, 3),
+('cuisine3', 'Cuisine rustique.', '/cuisine3.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Cuisine' AND bien_piece = 3), 0, 3),
+('chambre3', 'Chambre avec vue sur les montagnes.', '/chambre3.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Chambre double' AND bien_piece = 3), 0, 3),
+('sdb3', 'Salle de bain avec douche.', '/sdb3.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Salle de bain' AND bien_piece = 3), 0, 3);
 
 -- Maison de campagne
 INSERT INTO `pcs_photo` (`nom_photo`, `description_photo`, `chemin_photo`, `piece_photo`, `est_couverture`, `photo_bien_id`) VALUES
-('salon4', 'Salon confortable avec cheminée.', '/salon4.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Salon' AND bien_piece = @bien_id4), 1, @bien_id4),
-('cuisine4', 'Cuisine équipée et spacieuse.', '/cuisine4.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Cuisine' AND bien_piece = @bien_id4), 0, @bien_id4),
-('chambre4', 'Chambre avec lit double.', '/chambre4.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Chambre double' AND bien_piece = @bien_id4), 0, @bien_id4),
-('sdb4', 'Salle de bain moderne.', '/sdb4.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Salle de bain' AND bien_piece = @bien_id4), 0, @bien_id4);
+('salon4', 'Salon confortable avec cheminée.', '/salon4.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Salon' AND bien_piece = 4), 1, 4),
+('cuisine4', 'Cuisine équipée et spacieuse.', '/cuisine4.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Cuisine' AND bien_piece = 4), 0, 4),
+('chambre4', 'Chambre avec lit double.', '/chambre4.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Chambre double' AND bien_piece = 4), 0, 4),
+('sdb4', 'Salle de bain moderne.', '/sdb4.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Salle de bain' AND bien_piece = 4), 0, 4);
 
 -- Studio moderne
 INSERT INTO `pcs_photo` (`nom_photo`, `description_photo`, `chemin_photo`, `piece_photo`, `est_couverture`, `photo_bien_id`) VALUES
-('salon5', 'Espace salon moderne.', '/salon5.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Salon' AND bien_piece = @bien_id5), 1, @bien_id5),
-('cuisine5', 'Cuisine ouverte et fonctionnelle.', '/cuisine5.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Cuisine' AND bien_piece = @bien_id5), 0, @bien_id5),
-('sdb5', 'Salle de bain avec douche.', '/sdb5.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Salle de bain' AND bien_piece = @bien_id5), 0, @bien_id5);
+('salon5', 'Espace salon moderne.', '/salon5.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Salon' AND bien_piece = 5), 1, 5),
+('cuisine5', 'Cuisine ouverte et fonctionnelle.', '/cuisine5.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Cuisine' AND bien_piece = 5), 0, 5),
+('sdb5', 'Salle de bain avec douche.', '/sdb5.jpg', (SELECT id_piece FROM pcs_piece WHERE nom_piece = 'Salle de bain' AND bien_piece = 5), 0, 5);
 
 
 --
